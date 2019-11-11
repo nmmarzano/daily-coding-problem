e7371ec955dc355908ce92b0e9ce51a99cbf082a
Shou <nmmarzano@users.noreply.github.com>
2019-11-11 15:04:32 -0300

    Change tabs for spaces

--- sample-question/Staircase.java
+++ sample-question/Staircase.java
@@ -1,25 +1,24 @@
 public class Staircase {
+
+  public static int waysToClimb(int steps, int[] alts){
+    return recursiveWaysToClimb(steps, 0, alts, "");
+  }
 	
-	public static int waysToClimb(int steps, int[] alts){
-		return recursiveWaysToClimb(steps, 0, alts, "");
-	}
-	
-	private static int recursiveWaysToClimb(int steps, int climbed, int[] alts, String path){
-		if (steps - climbed == 0) {
-			System.out.println("Found path:"+path);
-			return 1;
-		} else if (steps - climbed < 0) {
-			return 0;
-		} else {
-			int count = 0;
-			for (int i = 0; i < alts.length; i++){
-				if(alts[i] != 0){
-					count += recursiveWaysToClimb(steps - climbed, alts[i], alts, path+" "+alts[i]);
-				}
-			}
-			return count;
-		}
-		
-	}
+  private static int recursiveWaysToClimb(int steps, int climbed, int[] alts, String path){
+    if (steps - climbed == 0) {
+      System.out.println("Found path:"+path);
+      return 1;
+    } else if (steps - climbed < 0) {
+      return 0;
+    } else {
+      int count = 0;
+      for (int i = 0; i < alts.length; i++){
+        if(alts[i] != 0){
+          count += recursiveWaysToClimb(steps - climbed, alts[i], alts, path+" "+alts[i]);
+        }
+      }
+      return count;
+    }		
+  }
 	
 }
