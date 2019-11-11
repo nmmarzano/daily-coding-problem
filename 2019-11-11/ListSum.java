9b1934537db72fe806370137b5c14520792f43d1
Shou <nmmarzano@users.noreply.github.com>
2019-11-11 14:55:21 -0300

    Create ListSum.java

---
+++ 2019-11-11/ListSum.java
@@ -0,0 +1,14 @@
+public class ListSum {
+	
+	public static boolean addsUp(int[] nums, int tgt){
+		for(int i = 0; i < nums.length; i++){
+			for(int j = i+1; j < nums.length; j++){
+				if(nums[i] + nums[j] == tgt){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+}
