dfa59f2cdc7f6f5fb6607e625d271414720d3eb7
Shou <nmmarzano@users.noreply.github.com>
2019-11-11 11:49:42 -0300

    Add better infinite recursion check

--- sample-question/Staircase.java
+++ sample-question/Staircase.java
@@ -1,9 +1,8 @@
+package dailycodingproblem;
+
 public class Staircase {
 	
 	public static int waysToClimb(int steps, int[] alts){
-		if (alts.length == 0 || (alts.length == 1 && alts[0] == 0)) {
-			return 0;
-		}
 		return recursiveWaysToClimb(steps, 0, alts, "");
 	}
 	
@@ -16,7 +15,9 @@ public class Staircase {
 		} else {
 			int count = 0;
 			for (int i = 0; i < alts.length; i++){
-				count += recursiveWaysToClimb(steps - climbed, alts[i], alts, path+" "+alts[i]);
+				if(alts[i] != 0){
+					count += recursiveWaysToClimb(steps - climbed, alts[i], alts, path+" "+alts[i]);
+				}
 			}
 			return count;
 		}
